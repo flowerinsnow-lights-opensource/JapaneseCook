11e3d01a2fb1937b0e3c7057f5eede570298e573
冬花ice <flowerinsnow@donghua.email.cn>
2023-01-10 02:59:59 +0000

    最好还是注入，不要修改，免得和别的<del>外挂</del>mod冲突

--- src/main/java/online/flowerinsnow/japanesecook/mixin/MixinModList.java
+++ src/main/java/online/flowerinsnow/japanesecook/mixin/MixinModList.java
@@ -1,30 +1,30 @@
 package online.flowerinsnow.japanesecook.mixin;
 
+import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.network.handshake.FMLHandshakeMessage;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import online.flowerinsnow.japanesecook.JapaneseCook;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
 import java.util.Map;
 
 @Mixin(FMLHandshakeMessage.ModList.class)
 @SideOnly(Side.CLIENT)
 public class MixinModList {
-    @Redirect(
+    @Shadow
+    private Map<String,String> modTags;
+
+    @Inject(
             method = "<init>(Ljava/util/List;)V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
-                    ordinal = 0
-            )
+            at = @At("RETURN")
     )
-    private static <K, V> V init(Map<K, V> instance, K k, V v) {
-        if (!k.equals(JapaneseCook.MODID)) {
-            return instance.put(k, v);
-        }
-        return null;
+    private void init(List<ModContainer> modList, CallbackInfo ci) {
+        modTags.remove(JapaneseCook.MODID);
     }
 }
